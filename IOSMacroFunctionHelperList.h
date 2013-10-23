f0e8a420d837d2a35c275cbb04dfb37b7a12a8b9
Michael Henry Pantaleon <michaelhenry119@yahoo.com.ph>
2013-10-23 16:46:20 +0800

    no message

--- IOSMacroFunctionHelperList.h
+++ IOSMacroFunctionHelperList.h
@@ -9,6 +9,9 @@
 // http://bmpradeep.wordpress.com/tag/uicolorfromrgb
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
+#define UIColorFromRGBWithAlpha(rgbValue,a) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:a]
+
+
 #define HeightForText(text,width,margin,font)   ([text sizeWithFont:font constrainedToSize:CGSizeMake(width-(margin*2),2000.0f) lineBreakMode:NSLineBreakByWordWrapping].height + margin*2)
 
 #define isSystemGreaterThanEqual(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
